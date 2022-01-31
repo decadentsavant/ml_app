3f45e2ffe0d6838db90dddb68e3edae4bff2bbfe
Corey Stewart <decadentsavant@gmail.com>
2022-01-31 05:47:30 -0500

    Consolidated home cubit and state.

--- lib/home/cubit/home_cubit.dart
+++ lib/home/cubit/home_cubit.dart
@@ -1,10 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
-part 'home_state.dart';
-
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(const HomeState());
 
   void setTab(HomeTab tab) => emit(HomeState(tab: tab));
+}
+
+enum HomeTab { newEntry, all, stats, account }
+
+class HomeState extends Equatable {
+  const HomeState({
+    this.tab = HomeTab.newEntry,
+  });
+
+  final HomeTab tab;
+
+  @override
+  List<Object> get props => [tab];
 }
