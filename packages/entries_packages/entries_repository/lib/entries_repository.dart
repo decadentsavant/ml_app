eb4d286f5b9e62c97c601723eb926926913cbd83
Corey Stewart <decadentsavant@gmail.com>
2022-02-12 11:58:41 -0500

    Exposed FrequencyType in repository export.

--- packages/entries_packages/entries_repository/lib/entries_repository.dart
+++ packages/entries_packages/entries_repository/lib/entries_repository.dart
@@ -1,4 +1,5 @@
 library entries_repository;
 
 export 'package:entries_api/entries_api.dart' show Entry;
+export 'package:entries_api/entries_api.dart' show FrequencyType;
 export 'src/entries_repository.dart';
