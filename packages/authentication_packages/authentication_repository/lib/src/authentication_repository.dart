e4f556400c3b7d29de5474cad9c37b076059d3fc
Corey Stewart <decadentsavant@gmail.com>
2022-03-15 07:27:53 -0400

    Created authentication_packages.

---
+++ packages/authentication_packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,7 @@
+/// {@template authentication_repository}
+/// Dart package which manages the authentication domain.
+/// {@endtemplate}
+class AuthenticationRepository {
+  /// {@macro authentication_repository}
+  const AuthenticationRepository();
+}
